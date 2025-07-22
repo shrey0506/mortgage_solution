7f2a75f57cf3b46dd61f9983696412315b47b733
shrey0506 <thesinhas.ss@gmail.com>
2025-07-22 08:53:47 +0530

    setting up code

---
+++ src/utils/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name __)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging. INFO)
+logger.addHandler (console_handler)
+console_handler.setFormatter(formatter)

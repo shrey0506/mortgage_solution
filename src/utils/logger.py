308f86c8229f8959e389999a3270039f820e9215
shrey0506 <thesinhas.ss@gmail.com>
2025-07-22 09:12:07 +0530

    added chat code

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,9 @@
 import logging
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-logger = logging.getLogger(__name __)
+logger = logging.getLogger(__name__)
+
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging. INFO)
-logger.addHandler (console_handler)
+console_handler.setLevel(logging.INFO)
+logger.addHandler(console_handler)
 console_handler.setFormatter(formatter)
